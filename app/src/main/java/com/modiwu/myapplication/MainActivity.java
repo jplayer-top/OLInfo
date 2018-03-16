b21b5d9bae080989beb511b5a7d6e437816b7feb
oblivion0001 <18366108542@163.com>
2018-03-16 11:55:35 +0800

    dialog

--- app/src/main/java/com/modiwu/myapplication/MainActivity.java
+++ app/src/main/java/com/modiwu/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import top.jplayer.baseprolibrary.ui.SuperBaseActivity;
 import top.jplayer.baseprolibrary.widgets.dialog.DialogNoviceGuide;
 import top.jplayer.baseprolibrary.widgets.dialog.DialogRedHb;
+import top.jplayer.baseprolibrary.widgets.dialog.DialogShare;
 import top.jplayer.baseprolibrary.widgets.dialog.DialogSign;
 import top.jplayer.baseprolibrary.widgets.dialog.DialogSubmitSure;
 
@@ -32,7 +33,7 @@ public class MainActivity extends SuperBaseActivity {
         beans.add("新手礼包");
         beans.add("抢红包");
         beans.add("签到");
-        beans.add("输入框");
+        beans.add("分享");
         beans.add(getString(R.string.app_name));
         beans.add(getString(R.string.app_name));
         beans.add(getString(R.string.app_name));
@@ -48,6 +49,8 @@ public class MainActivity extends SuperBaseActivity {
                 new DialogRedHb(this).show(R.id.ivOpen);
             } else if (position == 3) {
                 new DialogSign(this).show();
+            } else if (position == 4) {
+                new DialogShare(this).show();
             }
         });
     }
